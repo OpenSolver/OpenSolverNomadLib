4832bd2e548ccec245cdc1f35d193a605dd262f2
Jack Dunn <jack.dunn.nz@gmail.com>
2014-07-24 18:07:23 +1200

    Add support for building on Mac.

---
+++ Mac/build_lib.sh
@@ -0,0 +1,9 @@
+# Builds the NOMAD source and to static library and copies to OpenSolverNomadDylib project
+# Must be run from the containing folder
+
+# Make the NOMAD library
+cd ../src
+make
+
+# Copy to OpenSolverNomadDylib folder
+cp ../Mac/lib/nomad.a ../../OpenSolverNomadDylib/NOMADLib/libnomad.a
